79c9680b95182621d94f57ce7e17d5f06895f958
Tom Manning <tom@manningetal.com>
2026-06-27 06:43:33 -0400

    get_client_lists() gets full set of active clients

--- make_guest_lists.py
+++ make_guest_lists.py
@@ -6,7 +6,7 @@
 # ]
 # ///
 '''
-the 4 lists are: 
+the 4 lists used by make_label_pdfs() are:
   Delivery, 'Saturday': (7, 12), 'Friday-before-3': (12, 15), 'Friday-after-3': (15, 23)}
       where each list is:
         tuples with the guest's first name, last name, route or pickup time, and item count
@@ -85,7 +85,7 @@ def parse_visit_response(response_data, guest_lists, this_weeks_dates):
             am_pm = "PM"
          pickup_time_str = f"{pickup_hour:02d}:{pickup_minute:02d} {am_pm}"
 
-         client_tuple = (visit_dict['client_id'], None, pickup_time_str, item_count)
+         client_tuple = (visit_dict['client_id'], None, pickup_time_str, int(item_count))
          if date_str == this_weeks_dates[FRIDAY_IDX]:
             if pickup_hour < FRIDAY_SPLIT_REPORT_HOUR:
                guest_list_index = GUEST_LIST_IDX_E.Friday_before_3.value
@@ -107,11 +107,11 @@ def parse_visit_response(response_data, guest_lists, this_weeks_dates):
          print(f"guest list index is None: {visit_dict['visit_type']=} {visit_dict['id']=} {visit_dict['visit_datetime']=} {visit_dict['client_id']=}")
 
    if record_count != added_count:
-      print(f"parse visits results {record_count=} {added_count=} {done=}")
+      print(f"Error: parse_visit_response added {added_count} visits but the {record_count=}; {done=}")
    return done, guest_lists
 
-def get_visits(guest_lists, this_weeks_dates, token):
 
+def get_visits(guest_lists, this_weeks_dates, token):
    RECORD_LIMIT = 50
    MAX_PAGE_NUMBER = 10
 
@@ -139,14 +139,14 @@ def get_visits(guest_lists, this_weeks_dates, token):
       # next_page = response_list['next_page']
 
       done, guest_lists = parse_visit_response(response_list['data'], guest_lists, this_weeks_dates)
-      if 1: 
+      if 0: 
          print(f"{page_number=} list_lengths: ", end="")
          for guest_list_index in GUEST_LIST_IDX_E:
             print(f"{guest_list_index.name}={len(guest_lists[guest_list_index.value])} ", end="")
          print()
+
       if done:
          break
-
    return guest_lists
 
 def parse_client_response(response_list, client_info_dict):
@@ -155,8 +155,8 @@ def parse_client_response(response_list, client_info_dict):
    for individual_client_dict in response_list:
       record_count += 1
       client_id = individual_client_dict['id']
-      if record_count == 1:
-         print(f"DEBUG {individual_client_dict=}")
+      # if record_count == 1:
+      #    print(f"DEBUG {individual_client_dict=}")
 
       if 'delivery_route_name' in individual_client_dict:
          delivery_route = individual_client_dict['delivery_route_name']
@@ -175,12 +175,15 @@ def parse_client_response(response_list, client_info_dict):
          print(f"{client_id=} has no household members")
 
    if record_count != added_count:
-      print(f"parse clients results {record_count=} {added_count=}")
-   return client_info_dict
+      print(f"Error: parse_client_response() added {added_count} but there were {record_count} records")
+   return client_info_dict, added_count
 
-def get_client_lists(token, client_info_dict):
-   RECORD_LIMIT = 5
-   MAX_PAGE_NUMBER = 2
+def get_client_lists(token):
+   # as of July 2026, there are about 800 active clients
+   RECORD_LIMIT = 50
+   MAX_PAGE_NUMBER = 24
+
+   client_info_dict = {}
 
    url = "https://app.pantrysoft.com/api/v1/client/"
    params = {
@@ -203,23 +206,45 @@ def get_client_lists(token, client_info_dict):
          exit()
 
       response_list = response.json()
-      client_info_dict = parse_client_response(response_list['data'], client_info_dict)
+      client_info_dict, clients_added_count = parse_client_response(response_list['data'], client_info_dict)
+      if clients_added_count < RECORD_LIMIT:
+         break
 
    return client_info_dict
 
 if __name__ == "__main__":
    token = load_token()
 
-   client_info_dict = {}
-   client_info_dict = get_client_lists(token, client_info_dict)
-   print(f"DONE: {client_info_dict=}")
+   # the following dictionary is use to fill in the Last, First and Delivery Route when the guest_lists are generated
+   client_info_dict = get_client_lists(token)
+   MINIMUM_CLIENT_COUNT = 60
+   if len(client_info_dict) < MINIMUM_CLIENT_COUNT:
+      print(f"Error: number of clients is less than {MINIMUM_CLIENT_COUNT}")
+      exit()
+   print(f"DEBUG: {len(client_info_dict)} active clients found")
+   #client_info_dict={'1': ['Cindy', 'Walsh', '04 - JSM'], '2': ['Linda', 'Webb', '20 - Quaker'],
    exit()
 
-   this_weeks_dates = ["2026-05-22", "2026-05-23"]
+   this_weeks_dates = ["2026-06-26", "2026-06-27"]
+   # the four guest_lists enumerated by GUEST_LIST_IDX_E;  the array is created here and filled in by the function
    guest_lists = [[],[],[],[]]
    guest_lists = get_visits(guest_lists, this_weeks_dates, token)
 
-   PRINT_LISTS = True
+   ''' example list:
+   Saturday
+	0 ('1134', None, '10:45 AM', 34.0)
+	1 ('392', None, '10:30 AM', 37.0)
+	2 ('905', None, '10:00 AM', 40.0)
+	3 ('1299', None, '10:00 AM', 94.0)
+	4 ('1271', None, '09:45 AM', 28.0)
+	5 ('1305', None, '09:30 AM', 25.0)
+	6 ('1273', None, '09:15 AM', 52.0)
+   '''
+
+   # the pickup lists need to be sorted by last name, first name; the pickup time is not used in the tags
+   # the delivery list needs to be sorted by route, last, first
+
+   PRINT_LISTS = False
    if PRINT_LISTS:
       for list_idx, guest_list in enumerate(guest_lists):
          # guest_list.sort(key=lambda x: (x[2], x[0]))
